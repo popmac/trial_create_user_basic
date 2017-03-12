a9a861af5119a068a98cc69bb12b76016081ccb9
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-12 14:35:15 +0900

    Membersテーブルにhashed_passwordカラムを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311131150) do
+ActiveRecord::Schema.define(version: 20170312053332) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",                                     null: false
@@ -23,15 +23,16 @@ ActiveRecord::Schema.define(version: 20170311131150) do
   end
 
   create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "number",                        null: false
-    t.string   "name",                          null: false
+    t.integer  "number",                          null: false
+    t.string   "name",                            null: false
     t.string   "full_name"
     t.string   "email"
     t.date     "birthday"
-    t.integer  "gender",        default: 0,     null: false
-    t.boolean  "administrator", default: false, null: false
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.integer  "gender",          default: 0,     null: false
+    t.boolean  "administrator",   default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "hashed_password"
   end
 
 end

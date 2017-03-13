21addfc93d0afce458f43461b9a778fd9a581aa3
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-13 22:51:29 +0900

    adminのルーティングを追加した。

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,25 @@ Rails.application.routes.draw do
   get "internal_server_error" => "top#internal_server_error"
   get "lesson/:action(/:name)" => "lesson"
 
-  resources :members do
+  resources :members, only: [:index, :show] do
     collection { get "search" }
     resources :entries, only: [:index]
   end
-  resources :articles
+  resources :articles, only: [:index, :show]
   resources :entries do
-    member { patch 'like', 'unlike' }
-    collection { get 'voted' }
+    member { patch :like, :unlike }
+    collection { get :voted }
   end
   resource :session, only: [:create, :destroy]
   resource :account
+
+  namespace :admin do
+    root to: "top#index"
+    resources :members do
+      collection { get "search" }
+    end
+    resources :articles
+  end
+
   match "*anything" => "top#not_found", via: [:get, :post, :patch, :delete]
 end

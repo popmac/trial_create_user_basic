eee407c2884b882d2e1d96c5345a473e45955a16
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-13 18:17:46 +0900

    MemberImageの画像のアップロード用の属性を追加した。

--- app/models/member_image.rb
+++ app/models/member_image.rb
@@ -1,3 +1,22 @@
 class MemberImage < ApplicationRecord
   belongs_to :member
+
+  attr_reader :uploaded_image
+
+  def uploaded_image=(image)
+    self.content_type = convert_content_type(image.content_type)
+    self.data = image.read
+    @uploaded_image = image
+  end
+
+  private
+  def convert_content_type(ctype)
+    ctype = ctype.rstrip.downcase
+    case ctype
+    when "image/pjpeg" then "image/jpeg"
+    when "image/jpg" then "image/jpeg"
+    when "image/x-png" then "image/png"
+    else ctype
+    end
+  end
 end

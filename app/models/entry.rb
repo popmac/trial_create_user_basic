32c1696f8a6872300da65b2eab70cfa335cd52f7
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-13 17:26:34 +0900

    entries(ブログ記事)を絞り込むスコープを追加した。

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,4 +6,10 @@ class Entry < ApplicationRecord
   validates :title, presence: true, length: { maximum: 200 }
   validates :body, :posted_at, presence: true
   validates :status, inclusion: { in: STATUS_VALUES }
+
+  scope :common, -> { where(status: "public") }
+  scope :published, -> { where("status <> ?", "draft") }
+  scope :full, ->(member) {
+    where("member_id = ? OR status <> ?", member.id, "draft") }
+  scope :readable_for, ->(member) { member ? full(member) : common }
 end
